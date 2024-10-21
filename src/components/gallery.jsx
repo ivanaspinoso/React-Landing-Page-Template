51028df5ab4396de646dd9754c610be2ee4f514b
ivana spinoso <ivanaspinoso@MacBook-Air-de-Agustin.local>
2024-10-21 17:33:47 -0300

    espaciado entre foto y foto

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -50,6 +50,7 @@ export const Gallery = (props) => {
 
             .image-container {
               position: relative;
+              margin-bottom: 20px; /* Aumenta el espaciado entre las imágenes */
             }
           }
 
